1b1ef06a8f797306b4a537492c226564e96ecaba
Sai Nadkarni <sai2010nadkarni@gmail.com>
2021-02-01 17:55:46 -0600

    fix item counts and sort MIS values

--- MSApriori.py
+++ MSApriori.py
@@ -10,17 +10,7 @@ tempDict = dict()
 itemList = list()
 itemCount = dict()
 transactionList = list()
-
-with open(os.path.join(__location__, 'input.txt')) as f:
-	for trans in f:
-		transactionList.append(list())
-		tempDict = trans.replace(' ', '').split(',')
-		for item in tempDict:
-			item = int(item)
-			transactionList[len(transactionList) - 1].append(item)
-			if(itemCount.get(item)):
-				itemCount[item] = itemCount.get(item) + 1
-	transactionCount = len(transactionList)
+L = list()
 
 with open(os.path.join(__location__, 'parameters.txt')) as f:
 	for mis in f:
@@ -36,9 +26,34 @@ with open(os.path.join(__location__, 'parameters.txt')) as f:
 			mis_values[int(tempDict[0])] = float(tempDict[1])
 
 	items = sorted(mis_values, key=mis_values.__getitem__)
+	items.remove('rest')
 	for item in items:
 		itemList.append(item)
 		itemCount[item] = 0
+
+with open(os.path.join(__location__, 'input.txt')) as f:
+	for trans in f:
+		transactionList.append(list())
+		tempDict = trans.replace(' ', '').split(',')
+		for item in tempDict:
+			item = int(item)
+			transactionList[len(transactionList) - 1].append(item)
+			if(itemCount.get(item)):
+				itemCount[item] = itemCount.get(item) + 1
+			else:
+				itemCount[item] = 1
+	transactionCount = len(transactionList)
+
+mis_values = {k: v for k, v in sorted(mis_values.items(), key=lambda item: item[1])}
+
+# for i in range(len(itemList)):
+#     if(i == 0):
+#         L.append(itemList[0])
+#     else:
+#         if((itemCount.get(itemList[i]) / transactionCount) >= next(iter(mis_values.values()))):
+#             L.append(itemList[i])
+#     for i in range(len(L)):
+#         itemCount[L[i]] = i
 
 print(sdcValue)
 print(mis_values)
@@ -46,3 +61,4 @@ print(itemList)
 print(itemCount)
 print(transactionCount)
 print(transactionList)
+# print(L)
